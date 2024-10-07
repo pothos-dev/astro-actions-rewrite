5d2de680c31bc7a3e0cc3657aef1ed4719dfb2f0
Daniel Ebert <redefiance@gmail.com>
2024-10-07 15:05:19 +0200

    compare with POST

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,11 @@ import { defineMiddleware } from "astro:middleware"
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const { pathname, searchParams } = context.url
-  if (pathname == "/") {
-    // return context.rewrite(`/hidden?${searchParams}`)
-    return next(`/hidden?${searchParams}`)
+  if (pathname == "/action") {
+    return next(`/with-action?${searchParams}`)
+  }
+  if (pathname == "/post") {
+    return next(`/with-post?${searchParams}`)
   }
   return next()
 })
